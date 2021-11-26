c9d6d6581a4d4a183e2c189b8ceb91583c24e312
Annastark <89winteriscoming@gmail.com>
2021-11-26 16:26:47 +0100

    Incredibile XD

--- main.js
+++ main.js
@@ -16,23 +16,25 @@
 
 
 const app = new Vue({
-    el: '#root',
-    data: {
-        images:[ 
-            'img/01.jpg',
-            'img/02.jpg',
-            'img/03.jpg',
-            'img/04.jpg',
-            'img/05.jpg'
-        ],
-        currentImg: 0
-    },
-    methods: {
-        nextImage: function () {
-            this.currentImg++;
-        },
-        prevImage: function() {
-            this.currentImg--;
-        }
-    }
-});
+            el: '#root',
+            data: {
+                images: [
+                    'img/01.jpg',
+                    'img/02.jpg',
+                    'img/03.jpg',
+                    'img/04.jpg',
+                    'img/05.jpg'
+                ],
+                currentImg: 0
+            },
+            methods: {
+                nextImage: function () {
+                    this.currentImg++;
+                },
+                prevImage: function () {
+                    this.currentImg--;
+                },
+                selectImg: function (index) {
+                    this.currentImg = index;
+                },
+ } });
