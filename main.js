c4f98bbd7460ece73871888217437a903ba70369
Annastark <89winteriscoming@gmail.com>
2021-11-26 16:49:30 +0100

    Image

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ const app = new Vue({
             el: '#root',
             data: {
                 images: [
-                    'img/01.jpg',
-                    'img/02.jpg',
-                    'img/03.jpg',
-                    'img/04.jpg',
-                    'img/05.jpg'
+                    'img/avengers.jpg',
+                    'img/stranger.jpg',
+                    'img/starlord.jpg',
+                    'img/wanda.jpg',
+                    'img/ironman.jpg'
                 ],
                 currentImg: 0
             },
